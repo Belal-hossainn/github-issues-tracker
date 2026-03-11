7d0de6fa3dd8108adf0e2d1fbff7e00e3733aa5b
belal-hossainn <belalhossain.rn@gmail.com>
2026-03-12 01:40:46 +0600

    card container design issues handle

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
 const container = document.getElementById("issuesContainer")
 const modalContainer = document.getElementById("issueModal")
 
-
-
 // LOGIN
 
 function login(){
-
 const username = document.getElementById("username").value
 const password = document.getElementById("password").value
 
@@ -16,9 +13,7 @@ if(username === "admin" && password === "admin123"){
 
 document.getElementById("loginPage").classList.add("hidden")
 document.getElementById("dashboard").classList.remove("hidden")
-
 loadIssues()
-
 }else{
 alert("Wrong credentials")
 }
@@ -26,23 +21,18 @@ alert("Wrong credentials")
 }
 
 // LOADING SPINNER
-
 function showSpinner(){
-
 container.innerHTML = `
 <div class="col-span-4 flex justify-center">
 <span class="loading loading-spinner loading-lg"></span>
 </div>
 `
-
 }
 
 //active button
 function setActiveButton(activeId){
 
-    console.log(activeId)
 const buttons = document.querySelectorAll(".tab-btn")
-
 buttons.forEach(btn=>{
 btn.classList.remove("btn-primary")
 })
@@ -51,29 +41,21 @@ document.getElementById(activeId).classList.add("btn-primary")
 }
 
 // LOAD ALL ISSUES
-
 async function loadIssues(){
-
 setActiveButton("allBtn")
 showSpinner()
-
 const res = await fetch(url)
 const data = await res.json()
-
 displayIssues(data.data)
 
 }
 
 //load open issues
-
-async function loadOpenIssues(){ 
-    
+async function loadOpenIssues(){    
 setActiveButton("openBtn")
 showSpinner()
-
 const res = await fetch(url)
 const data = await res.json()
-
 const openIssues = data.data.filter(
 issue => issue.status === "open"
 )
@@ -81,14 +63,11 @@ displayIssues(openIssues)
 }
 
 //load closed issues
-
 async function loadClosedIssues(){ 
 setActiveButton("closedBtn")
 showSpinner()
-
 const res = await fetch(url)
 const data = await res.json()
-
 const closedIssues = data.data.filter(
 issue => issue.status === "closed"
 )
